38e02e8322f9ff3685d539c4beda187b2e3eb9f7
Opilane TTHK <opilane@tthk.ee>
2020-10-12 14:49:18 +0300

    first commit

---
+++ FunctionReturn/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FunctionReturn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your year of birth: ");
+            string userInput = Console.ReadLine();
+            int userAge = CalculateAge(userInput);
+            ValidateAge(CalculateAge(userInput));
+        }
+        public static int CalculateAge(string userInput)
+        {
+            int userAge = 2020 - Int32.Parse(userInput);
+            return userAge;
+        }
+        public static void ValidateAge(int age)
+        {
+            if(age < 18)
+            {
+                Console.WriteLine("You are not old enough to drive a car.");
+            }else
+            {
+                Console.WriteLine("Go get it!");
+            }
+        }
+    }
+}
---
+++ Week7Functions/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+
+namespace Week7Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your year of birth: ");
+            
+            Agecalculator();
+
+
+
+        }
+        public static void Agecalculator()
+        {
+            int userinput = Int32.Parse(Console.ReadLine());
+            Console.WriteLine($"your age is: {2020 - userinput}");
+        } 
+        
+        
+        
+       
+  
+    
+    
+    
+    }
+}
